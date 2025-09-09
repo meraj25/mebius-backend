2ca41588bbfbdc1eda26a7154fde654f4c7011dc
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-09-09 15:59:46 +0530

    .

--- src/application/payment.ts
+++ src/application/payment.ts
@@ -3,8 +3,6 @@ import util from "util";
 import Order from "../infrastructure/db/entities/Order";
 import stripe from "../infrastructure/stripe";
 import Product from "../infrastructure/db/entities/Product";
-import { Client } from "@clerk/express";
-
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -32,7 +30,7 @@ async function fulfillCheckout(sessionId: string) {
   ).populate<{
     items: { productId: Product; quantity: number }[];
   }>("items.productId");
-  
+
   if (!order) {
     throw new Error("Order not found");
   }
@@ -95,7 +93,7 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
   if (!order) {
     throw new Error("Order not found");
   }
-  
+
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
     line_items: order.items.map((item) => ({
@@ -108,10 +106,8 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
       orderId: req.body.orderId,
     },
   });
-  console.log(session.client_secret);
-res.send({ clientSecret: session.client_secret });
 
-
+  res.send({ clientSecret: session.client_secret });
 };
 
 export const retrieveSessionStatus = async (req: Request, res: Response) => {
